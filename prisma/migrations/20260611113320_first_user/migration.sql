bf2acbf35272cfc197294ff95c86536f7765e07b
Dmitriy Blinow <dmitriyblinow@mail.ru>
2026-06-12 17:41:58 +0300

    first commit

---
+++ prisma/migrations/20260611113320_first_user/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "display_name" TEXT NOT NULL,
+    "avatar" TEXT,
+    "bio" TEXT,
+    "last_seen_at" TIMESTAMP(3) NOT NULL,
+    "public_key" TEXT NOT NULL,
+    "is_verified" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
